b2f287b9bd8b039f0b897c5cf4ca11763a6bfc21
gamester543 <gamesterrex@gmail.com>
2018-09-03 22:23:18 -0700

    Webpack production mode

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+const compression = require("compression");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, "dist")));
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
