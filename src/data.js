7be7bd87ddf3c1dcd16c6fcb6ff534b23c922936
Robert Chen <robertchen@live.com>
2018-09-08 22:09:20 -0700

    Reformatted code

--- src/data.js
+++ src/data.js
@@ -1,35 +1,39 @@
 const data = {
-  "redpwn": {
+  redpwn: {
     events: [
-    {
-      date: "9/28/17",
-      title: "Founding",
-      desc: "Met TPA on FTW and started doing CTFs together."
-    },{
-      date: "11/18/17",
-      title: "Easy CTF",
-      desc: "Two person team. Placed top 30."
-    },{
-      date: "1/28/18",
-      title: "Blevy",
-      desc: "The god himself!"
-    },{
-      date: "5/28/18",
-      title: "PACTF",
-      desc: "Placed 3rd."
-    },{
-      date: "9/30/18",
-      title: "Pico CTF",
-      desc: "No information yet."
-    }
+      {
+        date: "9/28/17",
+        title: "Founding",
+        desc: "Met TPA on FTW and started doing CTFs together."
+      },
+      {
+        date: "11/18/17",
+        title: "Easy CTF",
+        desc: "Two person team. Placed top 30."
+      },
+      {
+        date: "1/28/18",
+        title: "Blevy",
+        desc: "The god himself!"
+      },
+      {
+        date: "5/28/18",
+        title: "PACTF",
+        desc: "Placed 3rd."
+      },
+      {
+        date: "9/30/18",
+        title: "Pico CTF",
+        desc: "No information yet."
+      }
     ],
     name: "Redpwn"
   }
 };
 
 Object.keys(data).forEach(key => {
-  const {events} = data[key];
-  events.forEach(event => event.date = new Date(event.date).valueOf());
+  const { events } = data[key];
+  events.forEach(event => (event.date = new Date(event.date).valueOf()));
 });
 
 export default data;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "styles/main.less"
+import "styles/main.less";
 import "material-icons";
 
 import $ from "jquery";
@@ -10,32 +10,39 @@ const PAGE_OFFSET = 50;
 import timeline from "./timeline";
 import timelineUtils from "./timelineUtils";
 
-$(function(){  
-  Object.keys(data).forEach((key) => {
+$(function() {
+  Object.keys(data).forEach(key => {
     $("#content").prepend(timeline(key, key));
-    
+
     const $rootElem = $(`#${key}`);
     const events = data[key].events;
-    
+
     timelineUtils.create($rootElem, events);
   });
-  
+
   $(window).scroll(() => {
     const scrollPos = $(document).scrollTop();
-    
+
     $(".scrollable").each((i, elem) => {
-      if(!$(elem).hasClass("last") && scrollPos > (i + 1) * PAGE_HEIGHT){
+      if (!$(elem).hasClass("last") && scrollPos > (i + 1) * PAGE_HEIGHT) {
         $(elem).addClass("scrolled");
-      }else{
+      } else {
         $(elem).removeClass("scrolled");
       }
-      
-      if(scrollPos > (i) * PAGE_HEIGHT){
+
+      if (scrollPos > i * PAGE_HEIGHT) {
         $(elem).addClass("viewed");
       }
     });
   });
-  
-  $(".scrollable").each((i, elem) => $(elem).css("margin-top", `${i * PAGE_HEIGHT + (i == 0? 0: PAGE_OFFSET)}px`));
-  $(".scrollable").each((i, elem) => $(elem).css("z-index", `${$(".scrollable").length - i}`));
+
+  $(".scrollable").each((i, elem) =>
+    $(elem).css(
+      "margin-top",
+      `${i * PAGE_HEIGHT + (i == 0 ? 0 : PAGE_OFFSET)}px`
+    )
+  );
+  $(".scrollable").each((i, elem) =>
+    $(elem).css("z-index", `${$(".scrollable").length - i}`)
+  );
 });
