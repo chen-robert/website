6150d05a109f6bcc1a7713fa0ca47ad51b38dac4
Robert Chen <robertchen@live.com>
2018-12-05 22:48:27 -0800

    Quick bugfix

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ if(process.env.NODE_ENV === "production"){
   console.log(process.env.NODE_ENV);
 }
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
