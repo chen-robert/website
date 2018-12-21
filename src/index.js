9cfaf0934add62ffcb4dcddeaa827939377fcdaa
Robert Chen <robertchen@live.com>
2018-12-20 22:08:50 -0800

    Added AI panel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "styles/main.less";
+import "materialize-css";
+import $ from "jquery";
+
+$(() => {
+  function isElementInViewport($elem) {
+    return $(document).scrollTop() + $(window).height() * 3 / 4 > $elem.offset().top;
+  }
+
+  function checkAnimation() {
+    $('.animate-listener').each((i, elem) => {
+      const $elem = $(elem);
+      if ($elem.hasClass('animate')) return;
+
+      if (isElementInViewport($elem)) {
+        $elem.addClass('animate');
+      }
+    });
+  }
+
+  // Capture scroll events
+  $(window).scroll(checkAnimation);
+  checkAnimation();
+});
