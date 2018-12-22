03b2a97330932cddf8792a35ec5d9798bc09fb94
Robert Chen <robertchen@live.com>
2018-12-21 22:46:19 -0800

    Ran formatter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import $ from "jquery";
 
 $(() => {
   function isElementInViewport($elem) {
-    return $(document).scrollTop() + $(window).height() * 3 / 4 > $elem.offset().top;
+    return (
+      $(document).scrollTop() + ($(window).height() * 3) / 4 >
+      $elem.offset().top
+    );
   }
 
   function checkAnimation() {
-    $('.animate-listener').each((i, elem) => {
+    $(".animate-listener").each((i, elem) => {
       const $elem = $(elem);
-      if ($elem.hasClass('animate')) return;
+      if ($elem.hasClass("animate")) return;
 
       if (isElementInViewport($elem)) {
-        $elem.addClass('animate');
+        $elem.addClass("animate");
       }
     });
   }
