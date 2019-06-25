4d7b5e7d77f515f54e99a5cd20abc34b2ad79e5e
Robert Chen <robertchen@live.com>
2019-06-25 00:13:19 -0700

    Refactored to views

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require("express");
-const path = require("path");
+const express = require("express"),
+ autoprefixer = require('express-autoprefixer'),
+ lessMiddleware = require('less-middleware');
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "dist")));
+const app = express();
+app.set("view engine", "ejs");
+
+const staticPath = __dirname + '/public';
+app.use(lessMiddleware(staticPath));
+app.use(autoprefixer({browsers: ["last 3 versions", "> 1%"], cascade: false}));
+app.use(express.static(staticPath));
+
+app.get("/", (req, res) => res.render("index"));
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
